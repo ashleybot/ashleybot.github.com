f6d29246a83abdd99b7f24090db9e13cb0340528
ashleybot <e.a.greer@gmail.com>
2014-04-01 12:18:52 -0400

    working on top-bar transistions

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+$(window).scroll(function(){
+  if($(window).scrollTop() <= 40){
+    $('.top-bar').removeClass('alt');
+  }else{
+    $('.top-bar').addClass('alt');
+  }
+});
